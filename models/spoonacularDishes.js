0b86013274f5456f1b9baeba8c6cf61b227dc42b
ericdavidanderson <burrej1@gmail.com>
2021-11-06 21:51:21 -0700

    touch up on syntax

--- models/spoonacularDishes.js
+++ models/spoonacularDishes.js
@@ -1,33 +1,35 @@
 //model to store recipes in format of api call to spoonacular with title, image and api call route.
-const {Model, DataTypes } = require('sequelize');
-const sequelize = require('../congig/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../congig/connection");
 
-class Recipe extends Model { }
+class Recipe extends Model {}
 
 Recipe.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            aitoIncriment: true,
-        },
-        },
-        recipe: {
-            type: DataTypes.STRING,
-            allowNull: false,
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      aitoIncriment: true,
+    },
+    recipe: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    DESCRIPTION: {
+      TYPE: DataTypes.TEXT,
+      allowNull: true,
+    },
+    apiCallRoute: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: "Recipe",
+  }
+);
 
-        },
-        DESCRIPTION: {
-            TYPE: DataTypes.TEXT,
-            allowNull: true,
-        },
-        apiCallRoute:{
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-
-
-        }
-    
-)
+module.exports = Recipe;
