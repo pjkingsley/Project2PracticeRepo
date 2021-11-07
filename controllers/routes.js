b4ee8aa04effdc44228ad8cbd678341f2d698b7b
ericdavidanderson <burrej1@gmail.com>
2021-11-06 18:04:48 -0700

    express dependency added and route placeholders started.

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,1 +1,10 @@
 //Routes here to transfer from database to index.
+const express = require('express');
+
+const Port = 3001;
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send(
+    );
+});
--- models/spoonacularDishes.js
+++ models/spoonacularDishes.js
@@ -1,1 +1,3 @@
 //model to store recipes in format of api call to spoonacular with title, image and api call route.
+
+var SpoonacularRecipe = 
