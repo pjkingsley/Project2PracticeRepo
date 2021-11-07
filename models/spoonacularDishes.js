beacbf7b2897ab5320be528561f38f02b64c7e8b
ericdavidanderson <burrej1@gmail.com>
2021-11-06 20:39:57 -0700

    updated file structure with router and model starter code.

--- models/spoonacularDishes.js
+++ models/spoonacularDishes.js
@@ -1,3 +1,33 @@
 //model to store recipes in format of api call to spoonacular with title, image and api call route.
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../congig/connection');
 
-var SpoonacularRecipe = 
+class Recipe extends Model { }
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            aitoIncriment: true,
+        },
+        },
+        recipe: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        DESCRIPTION: {
+            TYPE: DataTypes.TEXT,
+            allowNull: true,
+        },
+        apiCallRoute:{
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+
+        }
+    
+)
---
+++ models/userProfile.js
@@ -0,0 +1,58 @@
+const {Model, DataTypes } = require('sequelize');
+const bcrypt = require('../config/connection');
+const swquelize = require('../config/connection');
+
+class UserProfile extends Model{
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+UserProfile.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [7],
+                },
+            },
+
+        },
+        {
+            hooks: {
+                beforeCreate: async (newUserData) => {
+                    newUserData.password = await bcrypt.hash(newUserData.password, 12);
+                    return newUserData;
+                },// do we need an integer value here?
+            },
+            sequelize,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'UserProfile',
+        }
+);
+
+module.exports = UserProfile;
+
+    
