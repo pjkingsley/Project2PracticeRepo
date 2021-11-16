9d8610969eafd169aa770f3c9957c8170494632a
Peter Kingsley <peterjkingsley@hotmail.com>
2021-11-16 13:26:24 -0800

    server updates

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);//imp
 
 //Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
 //Set up sessions with cookies
 const sess = {
