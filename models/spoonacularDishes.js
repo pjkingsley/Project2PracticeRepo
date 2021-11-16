9d6638f6253f2c21ea979ce5566c4787818e1a28
ericdavidanderson <burrej1@gmail.com>
2021-11-16 13:23:13 -0800

    model property name change

--- models/spoonacularDishes.js
+++ models/spoonacularDishes.js
@@ -24,7 +24,7 @@ SpoonRecipe.init(
       type: DataTypes.INTEGER,
       allowNull: true,
     },
-    recipe_id: {
+    user_reference: {
       type: DataTypes.INTEGER,
       references: {
         model: "UserProfile",
