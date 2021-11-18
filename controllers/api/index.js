d5f842950dd8c8046d0ccd810294ecacf96f1cba
ericdavidanderson <burrej1@gmail.com>
2021-11-17 16:45:46 -0800

    routes to partials

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commRecipesRoutes = require('./commRecipesRoutes');
 const spoonRoutes = require('./spoonRoutes');
-
+const hamburgerLinks = require('./hamburgerLinks.js');
 router.use('/users', userRoutes);
 router.use("/com_cookbook", commRecipesRoutes);
 router.use("/spoon_routes", spoonRoutes);
+router.use("/u", hamburgerLinks )
 module.exports = router;
