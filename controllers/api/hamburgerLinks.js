e07d3d4a0e7f1058183788dd59584c301b912a00
Peter Kingsley <peterjkingsley@hotmail.com>
2021-11-24 18:05:07 -0800

    moved commCookbook.js to the api render route

--- controllers/api/hamburgerLinks.js
+++ controllers/api/hamburgerLinks.js
@@ -12,6 +12,37 @@ router.get("/homepage", async (req, res) => {
 //CommunityCookbook link
 router.get("/communityCookbook", async (req, res) => {
   try {
+    const retrieveRecipes = async () => {
+      const result = await fetch ('/api/comCookbook/recipes', {
+          method: 'GET',
+      });
+      const json = await result.json();
+      return json;
+  };
+  
+  const renderRecipe = (recipe) => {
+      const card = document.createElement('div');
+      const cardTitle = document.createElement('div');
+      const cardDescription = document.createElement('div');
+      const cardIngredients = document.createElement('div');
+      const cardInstructions = document.createElement('div');
+  
+      card.classList.add('card', 'p-5');
+      cardTitle.classList.add('card-header', 'card-title');
+      cardDescription.classList.add('card-body', 'p-5');
+      cardIngredients.classList.add('card-body', 'p-5');
+      cardInstructions.classList.add('card-body', 'p-5');
+  
+      cardTitle.innerHTML =`${recipe.title}`;
+      cardDescription.innerHTML = `${recipe.description}`;
+      cardIngredients.innerHTML = `${recipe.ingredients}`;
+      cardInstructions.innerHTML = `${recipe.instructions}`;
+  };
+  
+  
+  
+    retrieveRecipes().then((response) => response.forEach((recipe) => 
+      renderRecipe(recipe)));
     res.render("communityCookbook", { loggedIn: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
